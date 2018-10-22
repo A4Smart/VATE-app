d4f0a2f48d992e713bb6d20f85c500534f6d4a30
Marco Fincato <marcofincato777@gmail.com>
2018-10-22 12:49:14 +0200

    Added bip to TTS engine

--- app/src/main/java/it/a4smart/vate/common/TTS.java
+++ app/src/main/java/it/a4smart/vate/common/TTS.java
@@ -1,7 +1,13 @@
 package it.a4smart.vate.common;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+
+import static android.speech.tts.TextToSpeech.QUEUE_ADD;
 
 public class TTS {
     private static TTS instance = null;
@@ -13,10 +19,44 @@ public class TTS {
             ttsEnabled = status == TextToSpeech.SUCCESS;
             runnable.run();
         });
+
+        ttsEngine.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+
+            }
+
+            @Override
+            public void onDone(String utteranceId) {
+                endBeep();
+            }
+
+            @Override
+            public void onError(String utteranceId) {
+
+            }
+        });
     }
 
     public void speak(String textToSay) {
-        ttsEngine.speak(textToSay, TextToSpeech.QUEUE_ADD, null, "VATE_tts");
+        if (isSpeaking()) stop();
+        new Handler().postDelayed(this::startBeep, 500);
+        new Handler().postDelayed(() -> addToQueue(textToSay), 750);
+
+    }
+
+    public void addToQueue(String textToSay) {
+        ttsEngine.speak(textToSay, QUEUE_ADD, null, "VATE_tts");
+    }
+
+    private void startBeep() {
+        ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        toneGen1.startTone(ToneGenerator.TONE_PROP_BEEP, 50);
+    }
+
+    private void endBeep() {
+        ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        toneGen1.startTone(ToneGenerator.TONE_PROP_BEEP2, 300);
     }
 
     public boolean isEnabled() {
@@ -36,6 +76,7 @@ public class TTS {
     }
 
     public void stop() {
+        endBeep();
         ttsEngine.stop();
     }
 
